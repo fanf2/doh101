3c5668b136c59dc7ec886c2c0b1a197940335b4e
Tony Finch <dot@dotat.at>
2018-03-17 17:29:41 +0000

    doh: attempt to decode doh GET request

--- roles/openresty/files/doh.lua
+++ roles/openresty/files/doh.lua
@@ -1,7 +1,10 @@
 local inspect = require "inspect"
 local r = ngx.req
 
+require "base64url"
+
 ngx.HTTP_UNSUPPORTED_MEDIA_TYPE = 415
+ngx.HTTP_TEAPOT = 418
 
 local ct_doh = 'application/dns-udpwireformat'
 
@@ -10,7 +13,18 @@ local function err(n)
 end
 
 local function doh_get()
-   ngx.say("get")
+   local q = r.get_query_args()
+   ngx.log(ngx.ERR, inspect(q))
+   if q.ct ~= true -- present but no value
+   and q.ct ~= ct_doh then
+      return err 'HTTP_UNSUPPORTED_MEDIA_TYPE'
+   end
+   if not q.dns then
+      ngx.log(ngx.ERR, "bar")
+      -- what error to return in this case?
+      return err 'HTTP_TEAPOT'
+   end
+   ngx.say(ngx.decode_base64url(q.dns))
 end
 
 local function doh_post()
